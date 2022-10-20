b6188acb65abc0ddfbd0ca2291f486f4eb52fafc
Rgvii9 <ragavi.sageekaran@edu.esiee.fr>
2022-10-20 16:56:11 +0200

    box un peu brutal

--- ProjetGeometrie/Assets/Script/HalfEdge.cs
+++ ProjetGeometrie/Assets/Script/HalfEdge.cs
@@ -3,51 +3,51 @@ using UnityEditor;
 using UnityEngine;
 namespace HalfEdge
 {
-public class HalfEdge
-{
-public int index;
-public Vertex sourceVertex;
-public Face face;
-public HalfEdge prevEdge;
-public HalfEdge nextEdge;
-public HalfEdge twinEdge;
-}
-public class Vertex
-{
-public int index;
-public Vector3 position;
-public HalfEdge outgoingEdge;
-}
-public class Face
-{
-public int index;
-public HalfEdge edge;
-}
-public class HalfEdgeMesh
-{
-public List<Vertex> vertices;
-public List<HalfEdge> edges;
-public List<Face> faces;
-public HalfEdgeMesh(Mesh mesh)
-{ // constructeur prenant un mesh Vertex-Face en paramètre
-//magic happens
+    public class HalfEdge
+    {
+        public int index;
+        public Vertex sourceVertex;
+        public Face face;
+        public HalfEdge prevEdge;
+        public HalfEdge nextEdge;
+        public HalfEdge twinEdge;
+    }
+    public class Vertex
+    {
+        public int index;
+        public Vector3 position;
+        public HalfEdge outgoingEdge;
+    }
+    public class Face
+    {
+        public int index;
+        public HalfEdge edge;
+    }
+    public class HalfEdgeMesh
+    {
+        public List<Vertex> vertices;
+        public List<HalfEdge> edges;
+        public List<Face> faces;
+        public HalfEdgeMesh(Mesh mesh)
+        { // constructeur prenant un mesh Vertex-Face en paramètre
+          //magic happens
 
-}
-public Mesh ConvertToFaceVertexMesh()
-{
-Mesh faceVertexMesh = new Mesh();
-// magic happens
-return faceVertexMesh;
-}
-public string ConvertToCSVFormat(string separator = "\t")
-{
-string str = "";
-//magic happens
-return str;
-}
-public void DrawGizmos(bool drawVertices, bool drawEdges, bool drawFaces)
-{
-//magic happens
-}
-}
+        }
+        public Mesh ConvertToFaceVertexMesh()
+        {
+            Mesh faceVertexMesh = new Mesh();
+            // magic happens
+            return faceVertexMesh;
+        }
+        public string ConvertToCSVFormat(string separator = "\t")
+        {
+            string str = "";
+            //magic happens
+            return str;
+        }
+        public void DrawGizmos(bool drawVertices, bool drawEdges, bool drawFaces)
+        {
+            //magic happens
+        }
+    }
 }
---
+++ ProjetGeometrie/Assets/Script/MeshGeneratorQuad.cs
@@ -0,0 +1,359 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MeshGeneratorQuad : MonoBehaviour
+{
+    MeshFilter m_Mf;
+
+    void Start()
+    {
+        m_Mf = GetComponent<MeshFilter>();
+        m_Mf.mesh = CreateBox(new Vector3(4, 1, 3));
+    }
+
+    Mesh CreateStrip(int nSegments, Vector3 halfSize)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "strip";
+
+        Vector3[] vertices = new Vector3[(nSegments + 1) * 2];
+        int[] quads = new int[nSegments * 4];
+
+        int index = 0;
+        Vector3 leftTopPos = new Vector3(-halfSize.x, 0, halfSize.z);
+        Vector3 rightTopPos = new Vector3(halfSize.x, 0, halfSize.z);
+
+        // 1 boucle for pour remplir vertices
+        for (int i = 0; i < nSegments + 1; i++)
+        {
+            float k = (float)i / nSegments;
+
+            Vector3 tmpPos = Vector3.Lerp(leftTopPos, rightTopPos, k);
+            vertices[index++] = tmpPos; // vertice du haut
+            vertices[index++] = tmpPos - 2 * halfSize.z * Vector3.forward; // vertice du bas
+        }
+        // 1 boucle for pour remplir triangles
+        index = 0;
+        for (int i = 0; i < nSegments; i++)
+        {
+            quads[index++] = 2 * i;
+            quads[index++] = 2 * i + 2;
+            quads[index++] = 2 * i + 3;
+            quads[index++] = 2 * i + 1;
+        }
+
+        mesh.vertices = vertices;
+        mesh.SetIndices(quads, MeshTopology.Quads, 0);
+
+        return mesh;
+    }
+
+    /*
+    Mesh CreateGridXZ(int nSegmentsX,int nSegmentsZ, Vector3 halfSize)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "grid";
+
+        Vector3[] vertices = new Vector3[???];
+        int[] quads = new int[?????];
+
+        // ???????????
+
+        mesh.vertices = vertices;
+        mesh.SetIndices(quads,MeshTopology.Quads,0);
+
+        return mesh;
+    }
+*/
+
+    private void OnDrawGizmos()
+    {
+        if (!(m_Mf && m_Mf.mesh)) return;
+
+        Mesh mesh = m_Mf.mesh;
+        Vector3[] vertices = mesh.vertices;
+        int[] quads = mesh.GetIndices(0);
+
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 15;
+        style.normal.textColor = Color.red;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(vertices[i]);
+            Handles.Label(worldPos, i.ToString(), style);
+        }
+
+        Gizmos.color = Color.black;
+        style.normal.textColor = Color.blue;
+
+        for (int i = 0; i < quads.Length / 4; i++)
+        {
+            int index1 = quads[4 * i];
+            int index2 = quads[4 * i + 1];
+            int index3 = quads[4 * i + 2];
+            int index4 = quads[4 * i + 3];
+
+            Vector3 pt1 = transform.TransformPoint(vertices[index1]);
+            Vector3 pt2 = transform.TransformPoint(vertices[index2]);
+            Vector3 pt3 = transform.TransformPoint(vertices[index3]);
+            Vector3 pt4 = transform.TransformPoint(vertices[index4]);
+
+            Gizmos.DrawLine(pt1, pt2);
+            Gizmos.DrawLine(pt2, pt3);
+            Gizmos.DrawLine(pt3, pt4);
+            Gizmos.DrawLine(pt4, pt1);
+
+            string str = string.Format("{0} ({1},{2},{3},{4})",
+                i, index1, index2, index3, index4);
+
+            Handles.Label((pt1 + pt2 + pt3 + pt4) / 4.0f, str, style);
+
+        }
+    }
+
+    string ConvertToCSV(string separator)
+    {
+        if (!(m_Mf && m_Mf.mesh)) return "";
+
+        Vector3[] vertices = m_Mf.mesh.vertices;
+        int[] quads = m_Mf.mesh.GetIndices(0);
+
+        List<string> strings = new List<string>();
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 pos = vertices[i];
+            strings.Add(i.ToString() + separator
+                + pos.x.ToString("N03") + " "
+                + pos.y.ToString("N03") + " "
+                + pos.z.ToString("N03") + separator + separator);
+        }
+
+        for (int i = vertices.Length; i < quads.Length / 4; i++)
+            strings.Add(separator + separator + separator);
+
+        for (int i = 0; i < quads.Length / 4; i++)
+        {
+            strings[i] += i.ToString() + separator
+                + quads[4 * i + 0].ToString() + ","
+                + quads[4 * i + 1].ToString() + ","
+                + quads[4 * i + 2].ToString() + ","
+                + quads[4 * i + 3].ToString();
+        }
+
+        return "Vertices" + separator + separator + separator + "Faces\n"
+            + "Index" + separator + "Position" + separator + separator +
+            "Index" + separator + "Indices des vertices\n"
+            + string.Join("\n", strings);
+
+
+
+
+        /* Mesh CreateNormalizedGridXZ(int nSegmentsX, int nSegmentsZ, ComputePosDelegate computePos = null)
+         {
+             Mesh mesh = new Mesh();
+             mesh.name = "normalizedGrid";
+
+             Vector3[] vertices = new Vector3[(nSegmentsX + 1) * (nSegmentsZ + 1)];
+             int[] quads = new int[nSegmentsX * nSegmentsZ * 4];
+
+             //Vertices
+             int index = 0;
+             for (int i = 0; i < nSegmentsZ + 1; i++)
+             {
+                 float kZ = (float)i / nSegmentsZ;
+
+                 for (int j = 0; j < nSegmentsX + 1; j++)
+                 {
+                     float kX = (float)j / nSegmentsX;
+                     vertices[index++] = computePos != null ? computePos(kX, kZ) : new Vector3(kX, 0, kZ);
+                 }
+             }
+
+             index = 0;
+             //Quads
+             for (int i = 0; i < nSegmentsZ; i++)
+             {
+                 for (int j = 0; j < nSegmentsX; j++)
+                 {
+                     quads[index++] = i * (nSegmentsX + 1) + j;
+                     quads[index++] = (i + 1) * (nSegmentsX + 1) + j;
+                     quads[index++] = (i + 1) * (nSegmentsX + 1) + j + 1;
+                     quads[index++] = i * (nSegmentsX + 1) + j + 1;
+                 }
+             }
+
+             mesh.vertices = vertices;
+             mesh.SetIndices(quads, MeshTopology.Quads, 0);
+
+             return mesh;
+         }*/
+
+
+        /*void Start()
+        {
+            m_Mf = GetComponent<MeshFilter>();
+            //m_Mf.mesh = CreateStrip(7, new Vector3(4, 1, 3));
+            //m_Mf.mesh = CreateNormalizedGridXZ(7, 4);
+
+            //Cylindre
+            *//* m_Mf.mesh = CreateNormalizedGridXZ(20, 40,
+                 (kX, kZ) =>
+                 {
+                     float rho, theta, y;
+
+                     // coordinates mapping de (kX,kZ) -> (rho,theta,y)
+                     theta = kX * 2 * Mathf.PI;
+                     y = kZ * 6;
+                     //rho = 3 + .25f * Mathf.Sin(kZ*2*Mathf.PI*4) ;
+                     rho = m_Profile.Evaluate(kZ) * 2;
+                     return new Vector3(rho * Mathf.Cos(theta), y, rho * Mathf.Sin(theta));
+                     //return new Vector3(Mathf.Lerp(-1.5f, 5.5f, kX), 1, Mathf.Lerp(-2, 4, kZ));
+                 }
+                 );
+            *//*
+
+            // Sphère
+            m_Mf.mesh = CreateNormalizedGridXZ(10, 5,
+                (kX, kZ) =>
+                {
+                    float rho, theta, phi;
+
+                    // coordinates mapping de (kX,kZ) -> (rho,theta,phi)
+                    theta = kX * 2 * Mathf.PI;
+                    phi = kZ * Mathf.PI;
+                    rho = 2 + .55f * Mathf.Cos(kX * 2 * Mathf.PI * 8)
+                                    * Mathf.Sin(kZ * 2 * Mathf.PI * 6);
+                    //rho = 3 + .25f * Mathf.Sin(kZ*2*Mathf.PI*4) ;
+                    //rho = m_Profile.Evaluate(kZ) * 2;
+
+                    return new Vector3(rho * Mathf.Cos(theta) * Mathf.Sin(phi),
+                        rho * Mathf.Cos(phi),
+                        rho * Mathf.Sin(theta) * Mathf.Sin(phi));
+                    //return new Vector3(Mathf.Lerp(-1.5f, 5.5f, kX), 1, Mathf.Lerp(-2, 4, kZ));
+                }
+                );
+
+            GUIUtility.systemCopyBuffer = ConvertToCSV("\t");
+            Debug.Log(ConvertToCSV("\t"));
+        }*/
+    }
+    Mesh CreateQuad(Vector3 halfSize)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "quad";
+
+        Vector3[] vertices = new Vector3[4];
+        int[] triangles = new int[2 * 3];
+
+        vertices[0] = new Vector3(-halfSize.x, 0, -halfSize.z);
+        vertices[1] = new Vector3(-halfSize.x, 0, halfSize.z);
+        vertices[2] = new Vector3(halfSize.x, 0, halfSize.z);
+        vertices[3] = new Vector3(halfSize.x, 0, -halfSize.z);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+
+    Mesh CreateBox(Vector3 halfSize)
+    {
+        Mesh mesh = new Mesh();
+        mesh.name = "box";
+
+        Vector3[] vertices = new Vector3[8];
+
+        int nSegments = 6; //car 6 faces
+        int[] triangles = new int[(2 * 3 * nSegments) + 1];
+
+        vertices[0] = new Vector3(-halfSize.x, 0, -halfSize.z); //A
+        vertices[1] = new Vector3(-halfSize.x, 0, halfSize.z); //B
+        vertices[2] = new Vector3(halfSize.x, 0, halfSize.z); //C
+        vertices[3] = new Vector3(halfSize.x, 0, -halfSize.z); //D
+
+        vertices[4] = new Vector3(-halfSize.x, 3, -halfSize.z);
+        vertices[5] = new Vector3(-halfSize.x, 3, halfSize.z);
+        vertices[6] = new Vector3(halfSize.x, 3, halfSize.z);
+        vertices[7] = new Vector3(halfSize.x, 3, -halfSize.z);
+
+        int index = 0;
+
+        for (int i = 0; i < 2*nSegments; i++)
+        {
+            triangles[index++] = 2 * i;
+            Debug.Log("index = " + index);
+            triangles[index++] = 2 * i+1;
+            Debug.Log("index = " + index);
+            triangles[index++] = 2 * i+2;
+            Debug.Log("index = " + index);
+        }
+
+        /*
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        triangles[3] = 0;
+        triangles[4] = 2;
+        triangles[5] = 3;
+
+        triangles[6] = 0;
+        triangles[7] = 4;
+        triangles[8] = 5;
+
+        triangles[9] = 0;
+        triangles[10] = 5;
+        triangles[11] = 1;
+
+        triangles[12] = 6;
+        triangles[13] = 5;
+        triangles[14] = 1;
+
+        triangles[15] = 6;
+        triangles[16] = 1;
+        triangles[17] = 2;
+
+        triangles[18] = 7;
+        triangles[19] = 6;
+        triangles[20] = 2;
+
+        triangles[21] = 7;
+        triangles[22] = 2;
+        triangles[23] = 3;
+
+        triangles[24] = 5;
+        triangles[25] = 6;
+        triangles[26] = 7;
+
+        triangles[27] = 5;
+        triangles[28] = 7;
+        triangles[29] = 4;
+
+        triangles[30] = 4;
+        triangles[31] = 7;
+        triangles[32] = 3;
+
+        triangles[33] = 4;
+        triangles[34] = 3;
+        triangles[35] = 0;*/
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        return mesh;
+
+    }
+
+}
